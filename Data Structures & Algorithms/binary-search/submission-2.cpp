8c4ae16aa4cfca66f631b382dd1a92a62860fe34
Gaetano Dalius Ambrico <73881234+GaetanoDaliusAmbrico@users.noreply.github.com>
2026-04-21 11:19:31 +0200

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int mid = 0;
+        int min = 0;
+        int max = nums.size()-1;
+
+        if (nums[min] == nums[max]){
+            return (nums[min] == target) ? 0 : -1;
+        } 
+
+        while (max > min) {
+            mid = min + (max - min)/2;
+            if (nums[mid] > target) {
+                max = mid-1;
+            } else if (nums[mid] < target) {
+                min = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        return (nums[min] == target) ? min : -1;
+    };
+};
